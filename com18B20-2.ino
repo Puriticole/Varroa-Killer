a82edb336a291c26639df200f702900f88a298e9
spaceruche <112617884+spaceruche@users.noreply.github.com>
2022-10-13 11:44:46 +0200

    Add files via upload

---
+++ com18B20-2.ino
@@ -0,0 +1,46 @@
+
+// First we include the libraries
+#include <OneWire.h> 
+#include <DallasTemperature.h>
+/********************************************************************/
+// Data wire is plugged into pin 2 on the Arduino 
+#define ONE_WIRE_BUS 2 
+#define ONE_WIRE_2_BUS 15
+/********************************************************************/
+// Setup a oneWire instance to communicate with any OneWire devices  
+// (not just Maxim/Dallas temperature ICs) 
+OneWire oneWire(ONE_WIRE_BUS); 
+OneWire oneWire2(ONE_WIRE_2_BUS); 
+/********************************************************************/
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+DallasTemperature sensors2(&oneWire2);
+/********************************************************************/ 
+void setup(void) 
+{ 
+ // start serial port 
+ Serial.begin(115200); 
+ Serial.println("Dallas Temperature IC Control Library Demo"); 
+ // Start up the library 
+ sensors.begin();
+ sensors2.begin();
+} 
+void loop(void) 
+{ 
+ // call sensors.requestTemperatures() to issue a global temperature 
+ // request to all devices on the bus 
+/********************************************************************/
+ Serial.print(" Requesting temperatures..."); 
+ sensors.requestTemperatures(); // Send the command to get temperature readings
+ sensors2.requestTemperatures(); // Send the command to get temperature readings 
+ Serial.println("DONE"); 
+/********************************************************************/
+ Serial.println("Temperature is: "); 
+ Serial.print("le capteur 1 : ");
+ Serial.println(sensors.getTempCByIndex()); // Why "byIndex"?  
+   // You can have more than one DS18B20 on the same bus.  
+   // 0 refers to the first IC on the wire
+ Serial.print("le capteur 2 : "); 
+// Serial.println(sensors2.getTempCByIndex());
+   delay(1000); 
+} 
