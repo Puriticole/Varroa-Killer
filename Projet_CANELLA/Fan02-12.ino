87e0ae317e93b47f5c14bf4476059c4d57bc0730
spaceruche <112617884+spaceruche@users.noreply.github.com>
2022-12-20 08:32:13 +0100

    Update Fan02-12.ino

--- Projet_CANELLA/Fan02-12.ino
+++ Projet_CANELLA/Fan02-12.ino
@@ -1,23 +1,24 @@
-#include <OneWire.h>
-const int fanPin = 6;
-
-// PWM channel 0 parameter
-const int freq = 1; // 5000 Hz
-const int fanChannel = 0;
-const int resolution = 8; // 8-bit resolution
+int pwmChannel = 0; //Choisit le canal 0
+int frequence = 1000; //Fréquence PWM de 1 KHz
+int resolution = 8; // Résolution de 8 bits, 256 valeurs possibles
+int pwmPin = 12;
 
 void setup(){
-    // Configure the channel 0
-    ledcSetup(fanChannel, freq, resolution);
+  Serial.begin(115200);
+    // Configuration du canal 0 avec la fréquence et la résolution choisie
+    ledcSetup(pwmChannel, frequence, resolution);
 
-    // Attach the channel 0 on the 3 pins
-    ledcAttachPin(fanPin, fanChannel);
+    // Assigne le canal PWM au pin 23
+    ledcAttachPin(pwmPin, pwmChannel);
+
+    // Créer la tension en sortie choisi
+    ledcWrite(pwmChannel, 150); //1.65 V
+    pinMode(14,OUTPUT);
 }
 
 void loop(){
-    // Increase the brightness of the led in the loop
-    for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){
-        ledcWrite(fanChannel, dutyCycle);
-        delay(15);
-    }
+digitalWrite(14,!digitalRead(14));
+Serial.println(digitalRead(14));
+delay(100);
+
 }
